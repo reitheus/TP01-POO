051182b3d272311832b9b3edbfb6ada6e32342bc
Lou-LAE <136115381+Lou-LAE@users.noreply.github.com>
2025-01-29 17:46:34 -0300

    Update veiculo.cpp

--- Classes/veiculo.cpp
+++ Classes/veiculo.cpp
@@ -1,18 +1,14 @@
-// Definição de Biblioteca para classe Veículo
-#ifndef VEICULO_H
-#define VEICULO_H
+#include "Veiculo.h"
+#include <cstdlib>
 
-using namespace std;
+//Geração dos números aleatórios para os valores dos atributos
+Veiculo::Veiculo() {
+    ataque= rand() % 41 + 40;// Gera um número entre 40 e 80
+    blindagem = rand() % 41 + 30;// Gera um número entre 30 e 70
+    potencia = rand() % 31 + 20;// Gera um número entre 20 e 50
+}
 
-class Veiculo{
-    private: // atritubos da classe 
-        int ataque; 
-        int blindagem;
-        int potencia;
-
-    public: // métodos
-        Veiculo(); // construtor
-        int getPoderAtaque();
-};
-
-#endif
+//Cálculo do poder de ataque 
+int Veiculo::getPoderAtaque() {
+    return ((ataque* 5) + (blindagem * 4) + (potencia * 1)) / 10;
+}
