9a0c89d33545a0a93e3793ef84fe3300529bc64e
Lou-LAE <136115381+Lou-LAE@users.noreply.github.com>
2025-01-30 19:34:14 -0300

    Update veiculo.cpp

--- Classes/veiculo.cpp
+++ Classes/veiculo.cpp
@@ -3,12 +3,12 @@
 
 //Geração dos números aleatórios para os valores dos atributos
 Veiculo::Veiculo() {
-    ataque= rand() % 41 + 40;// Gera um número entre 40 e 80
+    poderAtaque= rand() % 41 + 40;// Gera um número entre 40 e 80
     blindagem = rand() % 41 + 30;// Gera um número entre 30 e 70
-    potencia = rand() % 31 + 20;// Gera um número entre 20 e 50
+    potenciadeFogo = rand() % 31 + 20;// Gera um número entre 20 e 50
 }
 
 //Cálculo do poder de ataque 
 int Veiculo::getPoderAtaque() {
-    return ((ataque* 5) + (blindagem * 4) + (potencia * 1)) / 10;
+    return ((poderAtaque* 5) + (blindagem * 4) + (potenciadeFogo * 1)) / 10;
 }
