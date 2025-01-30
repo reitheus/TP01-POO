96342b3c138aebf0ac39da9231ef828e3490a1fc
Lou-LAE <136115381+Lou-LAE@users.noreply.github.com>
2025-01-30 19:32:31 -0300

    Update aeronave.cpp

--- Classes/aeronave.cpp
+++ Classes/aeronave.cpp
@@ -3,12 +3,12 @@
 
 //Geração dos números aleatórios para os valores dos atributos
 Aeronave::Aeronave() {
-    ataque = rand() % 41 + 50;// Gera um número entre 50 e 90
-    manobra = rand() % 41 + 30;// Gera um número entre 30 e 70
+    poderAtaque = rand() % 41 + 50;// Gera um número entre 50 e 90
+    manobrabilidade = rand() % 41 + 30;// Gera um número entre 30 e 70
     alcance = rand() % 31 + 20;// Gera um número entre 20 e 50
 }
 
 //Cálculo do poder de ataque 
 int Aeronave::getPoderAtaque() {
-    return ((ataque * 5) + (manobra * 3) + (alcance * 2)) / 10;
+    return ((poderAtaque * 5) + (manobrabilidade * 3) + (alcance * 2)) / 10;
 }
