2937aa752bb6110425a1cf5f331f902aceaf2965
Henrique441 <123331740+Henrique441@users.noreply.github.com>
2025-02-01 00:05:55 -0300

    Add files via upload

---
+++ Classes/Aeronave.cpp
@@ -0,0 +1,14 @@
+#include "../Includes/Aeronave.h"
+#include <cstdlib>
+
+//Geração dos números aleatórios para os valores dos atributos
+Aeronave::Aeronave() {
+    poderAtaque = rand() % 41 + 50;// Gera um número entre 50 e 90
+    manobrabilidade = rand() % 41 + 30;// Gera um número entre 30 e 70
+    alcance = rand() % 31 + 20;// Gera um número entre 20 e 50
+}
+
+//Cálculo do poder de ataque 
+int Aeronave::getPoderAtaque() {
+    return ((poderAtaque * 5) + (manobrabilidade * 3) + (alcance * 2)) / 10;
+}
---
+++ Classes/Veiculo.cpp
@@ -0,0 +1,14 @@
+#include "../Includes/Veiculo.h"
+#include <cstdlib>
+
+//Geração dos números aleatórios para os valores dos atributos
+Veiculo::Veiculo() {
+    poderAtaque= rand() % 41 + 40;// Gera um número entre 40 e 80
+    blindagem = rand() % 41 + 30;// Gera um número entre 30 e 70
+    potenciadeFogo = rand() % 31 + 20;// Gera um número entre 20 e 50
+}
+
+//Cálculo do poder de ataque 
+int Veiculo::getPoderAtaque() {
+    return ((poderAtaque* 5) + (blindagem * 4) + (potenciadeFogo * 1)) / 10;
+}
